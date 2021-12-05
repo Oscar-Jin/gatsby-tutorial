e7d6ad241457a2e761c777620661f3f91157cfd5
Oscar Jin <kintininn@gmail.com>
2021-12-06 02:02:01 +0900

    complete gatsby tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // ───────────────────────────────────────────────────────────── import ───┐
 import * as React from "react";
 import Layout from "../components/layout.js";
+import { StaticImage } from "gatsby-plugin-image";
 
 // ───────────────────────────────────────────────────────────── define ───┐
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Hi there! I am Oscar Jin</p>
+      <StaticImage
+        src="https://images.unsplash.com/photo-1520121401995-928cd50d4e27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+        alt="green"
+      />
     </Layout>
   );
 };
